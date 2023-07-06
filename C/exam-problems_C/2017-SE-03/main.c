5bc2dcb23f18236d59b99c997cd2b1f5d99e5eae
Marina Dabova <107205614+marinadabova@users.noreply.github.com>
2023-07-06 12:02:29 +0300

    Update main.c

--- C/exam-problems_C/2017-SE-03/main.c
+++ C/exam-problems_C/2017-SE-03/main.c
@@ -2,11 +2,94 @@
 #include <stdlib.h>
 #include <err.h>
 #include <stdint.h>
-
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
 
 int main(int argc, char* argv[]){
+        if(argc != 4){
+                errx(1,"wrong number of arguments");
+        }
+        struct data{
+                uint16_t ofset;
+        uint8_t b1;
+        uint8_t b2;
+    }__attribute__((packed));
 
-	return 0;
+        struct data patch;
+
+        struct stat st1;
+        if(stat(argv[1], &st1) < 0){
+                err(3,"error stat");
+        }
+        if (st1.st_size % sizeof(patch) != 0){
+                 err(4,"patch file not in right format");
+        }
+        struct stat st2;
+
+        if(stat(argv[2], &st2) < 0){
+                 err(3,"error stat");
+        }
+        if(st2.st_size > 0xFFFF){
+                warnx("Too big file. Possible mistakes");
+    }
+
+        int p=open(argv[1], O_RDONLY);
+        if(p < 0){
+                err(2,"error while opening");
+        }
+        int fd1=open(argv[2], O_RDONLY);
+        if (fd1 < 0){
+                err(2,"error while opening");
+        }
+
+        int fd2=open(argv[3],O_CREAT| O_TRUNC, O_RDWR, S_IRUSR, S_IWUSR);
+        if (fd2 < 0){
+                err(2,"error while opening");
+        }
+
+        uint8_t buf;
+        ssize_t bytes_read;
+
+        while( (bytes_read=read(fd1, &buf, sizeof(buf))) >0){
+                if(write(fd2, &buf, bytes_read)< 0){
+                        err(5,"error while writing");
+                }
+        }
+
+        if (bytes_read == -1){
+                err(6,"error while reading");
+        }
+
+        while( (bytes_read=read(p, &patch, sizeof(patch))) > 0){
+                if(patch.ofset>st2.st_size){
+                        errx(7,"incorrect offset out of file");
+                }
+
+                if(lseek(fd2, patch.ofset,SEEK_SET) <0){
+                        err(8,"error lseek");
+                }
+                if(read(fd2, &buf, sizeof(buf)) <0){
+                        err(6,"error while reading");
+                }
+                if (buf != patch.b1){
+                        errx(9,"different pathc file");
+                }
+                if(lseek(fd2, patch.ofset, SEEK_SET) < 0){
+            err(8, "Error lseek");
+        }
+
+       if(write(fd2, &patch.b2, sizeof(patch.b2)) < 0){
+                err(6, "Error writnig");
+       }
+    }
+    if(bytes_read < 0){
+        err(6,"error while reading");
+    }
+        close(p);
+        close(fd1);
+        close(fd2);
+        return 0;
 }
 
 /*Зад. 64 2017-SE-03 Напишете програма на C, която приема три параметъра, имена на двоични файлове.
