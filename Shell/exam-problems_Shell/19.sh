b0c0896820234545e40bb6ac08ad50ba366a445b
Marina Dabova <107205614+marinadabova@users.noreply.github.com>
2023-07-26 12:00:37 +0300

    Update 19.sh

--- Shell/exam-problems_Shell/19.sh
+++ Shell/exam-problems_Shell/19.sh
@@ -3,19 +3,22 @@ if [[ ${#} -ne 2 ]] ;then
         echo "Number of arguments should be 2"
         exit 1
 fi
-
 if [[ ! -f ${1}   || ! -f ${2} ]] ;then
         echo "Arguments should be files"
         exit 2
+fi
+if  [[ ! "$1" =~ ^[a-zA-Z]+$ ]] ||  [[ ! "$2" =~ ^[a-zA-Z]+$ ]]; then
+    echo "Wrong format"
+    exit 3
 fi
 count1=$(cat ${1} | grep "${1}"| wc -l)
 count2=$(cat ${2} | grep "${2}"|wc -l)
 
 if [[ ${count1} -gt ${count2} ]] ;then
-        cat ${1}| cut -d ' ' -f 4- |sort > $1.songs
+        cat ${1}| cut -d ' ' -f 4- |sort > $1.songs   
+        # cat ${1}| cut -d ' ' -f 4- |sort > $(basename "$1").songs
 elif [[ ${count2} -gt ${count1} ]] ;then
         cat ${2}| cut -d ' ' -f 4- |sort > $2.songs
 else
         cat "No winner"
 fi
-
