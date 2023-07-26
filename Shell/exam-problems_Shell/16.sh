af4d7d99284618ece81ed7d3a77f8bc928af99c9
Marina Dabova <107205614+marinadabova@users.noreply.github.com>
2023-07-26 11:02:51 +0300

    Update 16.sh

--- Shell/exam-problems_Shell/16.sh
+++ Shell/exam-problems_Shell/16.sh
@@ -27,13 +27,15 @@ if [[ "$(whoami)" != "root" ]]; then
 fi
 
 for user in $(ps -e -o user= |sort |uniq); do
-        echo "$user"
+        #echo "$user"
         process=$(ps -u $user -o  pid=,rss= | sort -t ' ' -n -k 2  |awk '{count+=$2} END {print count, $1}')
         pid_max=$(echo $process| cut -d ' ' -f 2)
-        mem=$(echo $process |cut -d ' ' -f 1)
-       
-       	if [[ $mem -gt $1 ]]; then
-                 #kill -KILL $pid
-                 echo "KILL $pid_max of user: $user with rss: $mem"
+        mem_max=$(echo $process |cut -d ' ' -f 1)
+        echo "$user  $mem_max" 
+       	if [[ $mem_max -gt $1 ]]; then
+                #kill -TERM $pid_max
+                #sleep 2
+                #kill -KILL $pid_max
+                echo "KILL $pid_max of user: $user with rss: $mem_max"
         fi
 done
