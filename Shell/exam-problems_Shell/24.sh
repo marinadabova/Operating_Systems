5c3ba04b7cf45e23d07e83a8ec5e235c20ab7191
Marina Dabova <107205614+marinadabova@users.noreply.github.com>
2023-07-27 11:51:05 +0300

    Update 24.sh

--- Shell/exam-problems_Shell/24.sh
+++ Shell/exam-problems_Shell/24.sh
@@ -6,22 +6,23 @@
 #Забележка:За удобство приемаме, че ако има подаден втори параметър, то той е число.
 
 #!/bin/bash
-
-if [[ ! -d $1 ]]; then
-        echo "Wrong argument passed"
-        exit 1
-fi
-
 if [[ $# -gt 2 ]]; then
         echo "One or two argument should be passed"
         exit 2
 fi
+if [[ ! -d $1 ]]; then
+        echo "Wrong argument passed"
+        exit 2
+fi
 
-if [[ $# -eq 1 ]]; then
-        find  $1 -xtype l 2> /dev/null
-elif [[ $# -eq 2 && $2 =~ ^[[:digit:]]+$ ]];then
-        find $1 -type f -printf "%p %n\n" 2> /dev/null | awk -v hardlink=$2 '$2 >= hardlink {print $1,$2}'
+if [[ $# -eq 2 ]]; then
+       if  [[ ! "$2" =~ ^[0-9]+$ ]]; then
+		echo "not a num"
+		exit 3
+	fi
+        find $1 -type f -printf "%p %n\n" 2>/dev/null | awk -v hardlink=$2 '$2 >= hardlink {print $1}'
+      
 else
-        echo "Wrong input"
-        exit 3
+        find  $1 -xtype l 2> /dev/null
+        # find -L $1 -type l
 fi
