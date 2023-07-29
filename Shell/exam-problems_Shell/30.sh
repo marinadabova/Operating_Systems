fc8ec53e800828729d8d7fe91212abb08d5eaef3
Marina Dabova <107205614+marinadabova@users.noreply.github.com>
2023-07-29 10:12:39 +0300

    Update 30.sh

--- Shell/exam-problems_Shell/30.sh
+++ Shell/exam-problems_Shell/30.sh
@@ -29,7 +29,7 @@ while read line; do
         friend=$(dirname $line | sed -E 's/(.+)\/([^\/]+$)/\2/g' )
         num_lines=$(cat $line |wc -l)
 
-        if (cat $friend |egrep -q "friend" ); then
+        if (cat $friends |egrep -q "friend" ); then
 
                 old_num_lines=$(cat $friends | egrep "$friend" | awk '{print  $2}')
                 total_lines=$(echo "$num_lines + $old_num_lines" | bc)
@@ -43,6 +43,6 @@ while read line; do
 
 done< <(find $1 -type f |egrep "^[0-9]{4}\-[0-9]{2}\-[0-9]{2}\-[0-9]{2}\-[0-9 ]{2}\-[0-9]{2}\.txt$")
 
-cat $friends | sort -nr -k2
+cat $friends | sort -nr -k2 | head
 
 rm $friends
