af9a2e5e9e6c24dfd751121765eac807bc0041dd
Marina Dabova <107205614+marinadabova@users.noreply.github.com>
2023-08-03 10:51:58 +0300

    Update 54.sh

--- Shell/exam-problems_Shell/54.sh
+++ Shell/exam-problems_Shell/54.sh
@@ -127,3 +127,106 @@ else
         echo "invalid"
         exit 2
 fi
+#-----------------------
+#kr resh
+#!/bin/bash
+
+if [[ "$(whoami)" != "root" ]]; then
+        echo "You do not have permissions"
+        exit 1
+fi
+
+if (env | egrep -q "CTRSLOTS" ); then
+	slots=$(env | egrep "CTRSLOTS" | cut -d'=' -f2)
+else
+	slots="0"
+fi
+
+if [[ -z ${CTRSLOTS} ]]; then
+	CTRSLOTS=0
+fi
+
+if [[ ${1} == "autoconf ]]; then
+	echo "yes"
+else
+	for num in ${slots}; do
+		if [[ ${1} == "config" ]]; then
+			echo "graph_title SSA drive temperatures"
+			echo "graph_vlabel Celsius"
+			echo "graph_category sensors"
+			echo "graph_info This graph shows SSA drive temp"
+			
+			ssacli ctrl slot=$num pd all show detail | awk '$1=="Smart" {model=$3;slot=$6} \ 
+									$1 == "Array" {name=$2} \
+									$1 == "Unassigned" {name="UN"} \
+									$1 == "physicaldrive" {drive=$2;print
+ "SSA" slot model name drive".label SSA"slot,model,name,drive "\n" "SSA" slot model name drive".type GAUGE" }' | 
+sed 's/://;s/://' 
+		elif [[ $# -eq 0 ]]; then
+			ssacli ctrl slot=$num pd all show detail | awk "$1=="Smart" {model=$3;slot=$6}  
+				$1 == "Unassigned" {name=$2} 
+				$1 == "physicaldrive" {drive=$2}
+				$1 == "Current"	{temp=$4;print "SSA" slot model name drive ".value " temp}' |
+sed 's/://;s/://'
+		fi
+	done
+fi
+#-----------------------
+#nadq resh
+#!/bin/bash
+
+[ $(id -u) -eq 0 ] || exit 1
+
+if [ -z "${CTRLSLOTS}" ]; then
+    CTRLSLOTS=0
+fi
+
+if [[ $# -gt 1 ]]; then
+	echo "I expect 0 or 1 parameter"
+	exit 1
+fi
+
+function1() {
+	echo "graph_title SSA drive temperatures"
+	echo "graph_vlabel Celsius"
+	echo "graph_category sensors"
+	echo "graph_info This graph shows SSA drive temp"
+	cat ssa-input.txt | awk '$1 == "Smart" {controller=$3}
+			$5 == "Slot" {slot=$6}\
+			$1 == "Array" {array=$2}\
+			$1 == "Unassigned" {array="UN"}
+			$1 == "physicaldrive" {
+				pd=$2;
+				print "SSA" slot controller array pd ".label", "SSA" slot,controller,array,pd "\n" "SSA" slot controller array pd ".type","GAUGE"
+			}	
+		'| sed 's/://;s/://'	
+}
+
+function2() {
+	    cat ssa-input.txt | awk '$1 == "Smart" {controller=$3}
+              $5 == "Slot" {slot=$6}\
+              $1 == "Array" {array=$2}\
+              $1 == "Unassigned" {array="UN"}
+              $1 == "physicaldrive" {pd=$2}
+              $1 == "Current" { 
+              	temperature=$4
+                print "SSA" slot controller array pd ".value",temperature
+             }
+          '| sed 's/://;s/://'
+}
+
+
+if [[ $1 == "autoconf" ]]; then
+	echo "yes"
+elif [[ $1 == "config" ]]; then
+	for s in ${CTRLSLOTS}; do
+       ${SSACLI} ctrl slot=${s} pd all show detail
+    done | function1
+elif [[ $# -eq 0 ]]; then
+	for s in ${CTRLSLOTS}; do
+ 		${SSACLI} ctrl slot=${s} pd all show detail
+ 	done | function2
+else 
+	echo "invalid"
+	exit 2
+fi
