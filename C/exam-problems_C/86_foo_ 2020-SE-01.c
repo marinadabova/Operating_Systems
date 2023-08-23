1f21d561f74b1cc5e90f5b471c4f370b4051f10b
Marina Dabova <107205614+marinadabova@users.noreply.github.com>
2023-08-23 09:44:54 +0300

    mai e greshna- check-pitai nkg

--- C/exam-problems_C/86_foo_ 2020-SE-01.c
+++ C/exam-problems_C/86_foo_ 2020-SE-01.c
@@ -116,3 +116,139 @@ int main(int argc, char* argv[]){
 
         exit(0);
 }
+//***************************************************
+#foo.c
+
+#include <stdlib.h>
+#include <unistd.h>
+#include <err.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <errno.h>
+#include <fcntl.h>
+
+void close_safe(int fd);
+
+void close_safe(int fd) {
+    int errno_ = errno;
+    close(fd);
+    errno = errno_;
+}
+
+int main(int argc, char** argv) {
+    if (argc != 2) {
+        errx(1, "Invalid arguments. Usage: %s <file_name>", argv[0]);
+    }
+
+    const char* pipe_name = "./pipe.txt";
+    const char* file_name = argv[1];
+
+    if (mkfifo(pipe_name, 0666) == -1) {
+        err(3, "Could not mkfifo");
+    }
+
+    const pid_t child = fork();
+    if (child == -1) {
+        err(2, "Could not fork");
+    }
+
+    if (child == 0) {
+        int fd;
+        if ((fd = open(pipe_name, O_WRONLY)) == -1) {
+            err(8, "Could not open %s", pipe_name);
+        }
+
+        if (dup2(fd, 1) == -1) {
+            close_safe(fd);
+            err(3, "Could not dup2");
+        }
+        if (execlp("cat", "cat", file_name, (char*)NULL) == -1) {
+            close_safe(fd);
+            err(3, "Could not exec cat");
+        }
+    }
+    int status;
+    if (wait(&status) == -1) {
+        err(4, "Could not wait for child process to finish");
+    }
+
+    if (!WIFEXITED(status)) {
+        errx(5, "Child process did not terminate normally");
+    }
+
+    if (WEXITSTATUS(status) != 0) {
+        errx(6, "Child process finished with status not 0");
+    }
+
+    exit(0);
+}
+
+#bar.c
+
+#include <stdlib.h>
+#include <unistd.h>
+#include <err.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <errno.h>
+#include <fcntl.h>
+
+void close_safe(int fd);
+
+void close_safe(int fd) {
+    int errno_ = errno;
+    close(fd);
+    errno = errno_;
+}
+
+int main(int argc, char** argv) {
+    if (argc != 2) {
+        errx(1, "Invalid arguments. Usage: %s <file_name>", argv[0]);
+    }
+
+    const char* pipe_name = "./pipe.txt";
+    const char* command = argv[1];
+
+    const pid_t child = fork();
+    if (child == -1) {
+        err(2, "Could not fork");
+    }
+
+    if (child == 0) {
+        int fd;
+        if ((fd = open(pipe_name, O_RDONLY)) == -1) {
+            err(8, "Could not open %s", pipe_name);
+        }
+
+        if (dup2(fd, 0) == -1) {
+            close_safe(fd);
+            err(3, "Could not dup2");
+        }
+
+        if (execl(command, command, (char*)NULL) == -1) {
+            close_safe(fd);
+            err(3, "Could not exec cat");
+        }
+    }
+
+    int status;
+    if (wait(&status) == -1) {
+        err(4, "Could not wait for child process to finish");
+    }
+
+    if (!WIFEXITED(status)) {
+        errx(5, "Child process did not terminate normally");
+    }
+
+    if (WEXITSTATUS(status) != 0) {
+        errx(6, "Child process finished with status not 0");
+    }
+
+    if (unlink(pipe_name) == -1) {
+        err(7, "Could not unlink");
+    }
+
+    exit(0);
+}
