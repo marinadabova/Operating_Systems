08bf74d4a086d1d644fafe561131f47a2c088a89
Marina Dabova <107205614+marinadabova@users.noreply.github.com>
2023-08-23 09:19:47 +0300

    Update 86_foo_ 2020-SE-01.c

--- C/exam-problems_C/86_foo_ 2020-SE-01.c
+++ C/exam-problems_C/86_foo_ 2020-SE-01.c
@@ -52,3 +52,67 @@ int main(const int argc, const char* const argv[]) {
 която да обработи получените през тръбата данни, четейки от стандартен вход. Еквивалент на
 горния пример би било следното изпълнение:
 cat a.txt | /usr/bin/sort*/
+
+
+//niq
+зад.74
+foo.c
+#include <err.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+
+int main(int argc, char* argv[]){
+        if(argc != 2){
+                errx(1, "We need one argument");
+        }
+
+        if(mkfifo("./myfifo", 0666) < 0){
+                err(2, "Error mkfifo");
+        }
+
+        int fd = open("./myfifo", O_WRONLY);
+        if(fd < 0){
+                err(3, "Error opening");
+        }
+
+        dup2(fd, 1);
+
+        if(execlp("cat", "cat", argv[1], (char*)NULL) < 0){
+                err(4, "Error exec");
+        }
+
+        exit(0);
+}
+
+bar.c
+#include <err.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+
+
+int main(int argc, char* argv[]){
+        if(argc != 2){
+                errx(1, "We need one argument");
+        }
+
+        int fd = open("./myfifo", O_RDONLY);
+        if(fd < 0){
+                err(2, "Error opening");
+        }
+
+        dup2(fd, 0);
+
+        if(execlp(argv[1], argv[1], (char*)NULL) < 0){
+                err(3, "Error exec");
+        }
+
+        exit(0);
+}
