78c6f57cc5704b5777b9b5f7d6aabc409b6bea4e
Marina Dabova <107205614+marinadabova@users.noreply.github.com>
2023-11-18 12:31:03 +0200

    Update and rename 16.sh to 16_2016-SE-02.sh

--- Shell/exam-problems_Shell/16_2016-SE-02.sh
+++ Shell/exam-problems_Shell/16_2016-SE-02.sh
@@ -10,6 +10,36 @@
 #root 3 0.0 0.0 0 0 ? S May12 0:02 [ksoftirqd/0]
 #Забележка: Алтернативно може да ползвате изхода от ps -e -o uid,pid,rss
 
+#ot ypr 
+#!/bin/bash
+
+if ! [ $# -eq 1 ]; then
+        echo "Wrong number of arg"
+        exit 1
+fi
+IS_ROOT=0
+if [ $(whoami) == "root" ]; then
+        IS_ROOT=1
+fi
+if ! echo $1 |grep -q "^[1-9][0-9]*$" ; then
+        echo "Pls enter num"
+        exit 1
+
+for user in $(cat /etc/passwd | cut -d ':' -f 1); do
+        rss=$(ps -u $user -o rss= | awk '{sumRSS+=$1} END {print sumRSS}')
+        #pid=$(ps -u $user -o pid= )
+
+        if [ $IS_ROOT -eq 1 ]; then
+                if [ $1 -gt $rss ]; then
+                        pid=$(ps -u $user -o pid=,rss= | sort -k2 -n | cut -d ' ' -f1| head -1)
+
+                        echo kill -9 $pid
+                fi
+        fi
+done
+
+###########################
+
 #!/bin/bash
 if [[ ${#} -ne 1 ]]; then
         echo "It should be one parameter"
@@ -39,3 +69,5 @@ for user in $(ps -e -o user= |sort |uniq); do
                 echo "KILL $pid_max of user: $user with rss: $mem_max"
         fi
 done
+#########################
+
