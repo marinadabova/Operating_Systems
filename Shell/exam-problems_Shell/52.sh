a98e38fda0199f4cb549fb640211654a14a25613
Marina Dabova <107205614+marinadabova@users.noreply.github.com>
2024-03-30 17:29:42 +0200

    Update 52.sh

--- Shell/exam-problems_Shell/52.sh
+++ Shell/exam-problems_Shell/52.sh
@@ -62,3 +62,27 @@ while read line; do
                 fi
         fi
 done< <(cat $1| egrep -v "^#.*$")
+###########3
+#!/bin/bash
+
+if [ "${#}" -ne 1 ]; then
+        echo "One arg needed"
+        exit 1
+fi
+
+if [ ! -f "${1}" ]; then
+        echo "Argument should be file"
+        exit 2
+fi
+
+while read DEVICE STATE; do
+        if ! grep -q "${DEVICE}" /proc/acpi/wakeup ; then #sravnqvame error code-a
+                echo "WARN: ${DEVICE} not in wakeup" >&2
+                continue
+        fi
+        CURSTATE=$(cat /proc/acpi/wakeup |grep ${DEVICE}  | sed -E 's/^.*\*(.*) .*$/\1/')
+        if [ "${CURSTATE}" != "${STATE}" ]; then
+                echo "$DEVICE" >> "/proc/acpi/wakeup"
+
+        fi
+done< <(cat "${1}" | cut -d '#' -f 1 | grep -v "^ *$")
