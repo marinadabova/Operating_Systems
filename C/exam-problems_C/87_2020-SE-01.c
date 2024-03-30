2df5f72ce7b488d4e42a14df27c5497b01f19875
Marina Dabova <107205614+marinadabova@users.noreply.github.com>
2024-03-30 17:51:39 +0200

    Update 87_2020-SE-01.c

--- C/exam-problems_C/87_2020-SE-01.c
+++ C/exam-problems_C/87_2020-SE-01.c
@@ -143,7 +143,67 @@ int main(int argc, char* argv[]){
         return 0;
 }
 
+/********************/
+//ot chasa
+#include <stdio.h>
+#include <unistd.h>
+#include <err.h>
+#include <fcntl.h>
+#include <stdint.h>
 
+
+
+int main (int argc, char* argv[]){
+
+        if (argc != 3){
+                errx(1,"Two arg needed");
+        }
+
+        int pd[2];
+        if (pipe(pd) == -1){
+                err(2,"error while piping");
+        }
+        pid_t p=fork();
+
+        if (p == -1){
+                err(3,"error during fork");
+        }
+        else if(p == 0){
+                close(pd[0]);
+                dup2(pd[1],1);
+                execlp("cat", "cat", argv[1], (char*) NULL);
+        }
+        close(pd[1]);
+
+        int fd = open(argv[2], O_CREAT  |O_WRONLY | O_TRUNC , S_IRWXU);
+        uint8_t buf;
+        while (read(pd[0], &buf, sizeof(buf)) > 0 ){
+                if (buf == 0x55){
+                        continue;
+                }
+                if (buf == 0x7D){
+
+                        if (read(pd[0], &buf, sizeof(buf)) != sizeof(buf)){
+                                err(4,"error while reading");
+                        }
+
+
+                        uint8_t temp = buf ^ 0x20;
+                        if (temp != 0x00 && temp != 0x55 && temp != 0xFF && temp != 0x70){
+                                errx(5,"wrong file format");
+                        }
+                        write (fd, &temp,  sizeof(temp));
+
+                }
+                else{
+                        write(fd, &buf, sizeof(buf));
+                }
+        }
+        close(fd);
+        close(pd[0]);
+
+        return 0;
+}
 /*Зад. 87 2020-SE-02 При изграждане на система за пренасяне на сериен асинхронен сигнал върху ради-
 опреносна мрежа се оказало, че големи поредици от битове само нули или само единици смущават
 сигнала, поради нестабилно ниво. Инженерите решили проблема, като:
