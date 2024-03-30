3d0953f412527bec4965baf3ae810816f662cb86
Marina Dabova <107205614+marinadabova@users.noreply.github.com>
2024-03-30 17:05:06 +0200

    Update 27_2017-SE-04.sh

--- Shell/exam-problems_Shell/27_2017-SE-04.sh
+++ Shell/exam-problems_Shell/27_2017-SE-04.sh
@@ -39,3 +39,61 @@ if [[ $# -eq 2 ]];then
 else
         cat $temp
 fi
+#######################
+#!/bin/bash
+
+temp=$(mktemp)
+while read line
+do
+        stat $line | head -1 | sed -E 's/(^[^\/+])(\/.+)/\2/g' >> $temp
+
+done < <(find $1 -type l 2> /dev/null | egrep -v "$(find -L $1 -type l)")
+
+echo "Broken Symlinks: $(find -L $1 -type l | wc -l)" >> $temp
+
+#readlink
+if [[ $# -eq 2 ]];then
+        cat $temp > $2
+else
+        cat $temp
+fi
+########################
+#!/bin/bash
+
+if [[ ${#} -lt 1 ]]; then
+        echo "expecting 1 argument at least."
+        exit 1
+fi
+
+dir="$1"
+f="$2"
+
+if [ ! -d "$dir" ]; then
+    echo "Error: directory does not exist" >&2
+    exit 1
+fi
+
+
+
+broken_symlink=$(find -L "${dir}" -type l| wc -l)
+
+solution() {
+        while read link; do
+                linked_to=$(readlink ${link})
+                if [[ -e "${link}" ]]; then
+                        echo "$(basename ${link}) -> ${linked_to}"
+                fi
+
+        done < <(find ${dir} -type l)
+
+        echo "Broken symlinks: ${broken_symlink}"
+}
+if [[ ${#} -eq 2 ]]; then
+        if [[ -f  ${f} ]]; then
+                solution >> "${f}"
+    else
+        echo "File doesn't exist"
+    fi
+else
+        solution
+fi
